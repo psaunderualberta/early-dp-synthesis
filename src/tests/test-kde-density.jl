9ec2b9c22b27886dae975074c7919267cc352649
Paul Saunders <psaunder@MacBook-Pro.local>
2024-05-22 18:32:44 -0600

    kernel density estimation is running?

--- src/tests/test-kde-density.jl
+++ src/tests/test-kde-density.jl
@@ -7,4 +7,12 @@ https://github.com/JuliaStats/KernelDensity.jl
 using KernelDensity
 using Distributions
 
+function test_kde()
+    x = rand(Normal(0, 1), 1000)
+    
+    fn = kde(x)
+    println(pdf(fn, x))
+    println(pdf(fn, 0))
+end
 
+test_kde()
