6dfa14355a23b71294b28c7461df0051667e89dc
Paul Saunders <psaunder@MacBook-Pro.local>
2024-06-03 16:11:33 -0600

    Threads -> Processes

--- src/early-dp-synthesis.jl
+++ src/early-dp-synthesis.jl
@@ -5,61 +5,67 @@ using Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
 Pkg.instantiate()
 
-# @everywhere begin
-#     using Pkg
-#     Pkg.activate(joinpath(@__DIR__, ".."))
-#     Pkg.instantiate()
+@everywhere begin
+    using Pkg
+    Pkg.activate(joinpath(@__DIR__, ".."))
+    Pkg.instantiate()
+end
 
-using MLJ
-using Distributions
-using SymbolicRegression
-using ArgParse
+@everywhere begin
+    using MLJ
+    using Distributions
+    using SymbolicRegression
+    using ArgParse
 
-include("Losses.jl")
+    include("Losses.jl")
 
-function main(args)
-    accest = haskey(accuracy_estimators, args["accuracy"]) ? accuracy_estimators[args["accuracy"]] : error("A")
-    privest = haskey(privacy_estimators, args["privacy"]) ? privacy_estimators[args["privacy"]] : error("B")
-    combest = haskey(combiners, args["combiner"]) ? combiners[args["combiner"]] : error("C")
+    function main(args)
+        accest = haskey(accuracy_estimators, args["accuracy"]) ? accuracy_estimators[args["accuracy"]] : error("A")
+        privest = haskey(privacy_estimators, args["privacy"]) ? privacy_estimators[args["privacy"]] : error("B")
+        combest = haskey(combiners, args["combiner"]) ? combiners[args["combiner"]] : error("C")
 
-    # Dataset with two named features
-    n = 1000
+        # Dataset with two named features
+        n = 1000
 
-    # Ablee to use variables as keys
-    d = Dict(
-        "zero" => zeros(n),
-        SENSITIVITY_COLUMN_NAME => fill(args["sensitivity"], n),
-    )
+        # Ablee to use variables as keys
+        d = Dict(
+            "zero" => zeros(n),
+            SENSITIVITY_COLUMN_NAME => fill(args["sensitivity"], n),
+        )
 
-    X =  NamedTuple(((Symbol(key), value) for (key, value) in d))
-    y = @. zeros(Float64, n)
+        X =  NamedTuple(((Symbol(key), value) for (key, value) in d))
+        y = @. zeros(Float64, n)
 
-    # Define uniform
-    unif(a::T, b::T) where {T} = a < b ? rand(Uniform(a, b)) : T(NaN)
-    normal(a, b) = b > 0 ? rand(Normal(a, b)) : NaN
-    # laplace(b) = b > 0 ? rand(Laplace(0, b)) : NaN
+        # Define uniform
+        unif(a::T, b::T) where {T} = a < b ? rand(Uniform(a, b)) : T(NaN)
+        normal(a, b) = b > 0 ? rand(Normal(a, b)) : NaN
+        # laplace(b) = b > 0 ? rand(Laplace(0, b)) : NaN
 
-    # Define loss function
-    loss(tree, dataset, options) = privacy_loss(accest(), privest(), combest(), tree, dataset, options)
-    processes = nprocs() > 1 ? procs()[2:end] : procs()
+        # Define loss function
+        loss(tree, dataset, options) = privacy_loss(accest(), privest(), combest(), tree, dataset, options)
+        processes = nprocs() > 1 ? procs()[2:end] : procs()
 
-    # Define model
-    model = SRRegressor(;
-        save_to_file=false,
-        niterations=5,
-        binary_operators=[+, -, unif, normal],
-        unary_operators=[],
-        loss_function=loss,
-        maxdepth=10,
-        progress=true,
-    )
+        # Define model
+        model = SRRegressor(;
+            save_to_file=false,
+            parallelism=:multiprocessing,
+            procs=processes,
+            timeout_in_seconds=60 * 10, # 10 minutes
+            niterations=5,
+            binary_operators=[+, -, unif, normal],
+            unary_operators=[],
+            loss_function=loss,
+            maxdepth=10,
+            progress=true,
+        )
 
-    # Train model
-    mach = machine(model, X, y)
-    fit!(mach)  # Not sure why this says 'possible method call error'
+        # Train model
+        mach = machine(model, X, y)
+        fit!(mach)  # Not sure why this says 'possible method call error'
 
-    # Print report
-    println(report(mach))
+        # Print report
+        println(report(mach))
+    end
 end
 
 
@@ -82,6 +88,10 @@ function parse_commandline()
             help = "The sensitivity of the dataset"
             arg_type = Float64
             default = 1.0
+        "--timeout"
+            help = "The maximum time to run the symbolic regression model, in seconds"
+            arg_type = Int
+            default = 60 * 10  # 10 minutes
     end
 
     return parse_args(s)
